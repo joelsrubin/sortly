7156810f45df932e65b512edeba6ef61b00152bc
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-04-20 22:36:12 -0400

    add types

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import path from 'node:path';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
