80085a1b596acd86460ea943c791f05a703f017a
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-04-20 22:33:54 -0400

    update import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import path from 'path';
+import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
